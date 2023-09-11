aa4633aff5be68a5c7df88ca838d18275c53bd0e
Jonathan Flores <jf223mu@student.lnu.se>
2023-09-11 10:50:29 +0200

    Add test case

--- src/module/drawUtils.js
+++ src/module/drawUtils.js
@@ -1,11 +1,46 @@
 /**
  *
  * @param ctx
- * @param x
- * @param y
- * @param width
- * @param height
- * @param color
+ * @param canvas
+ * @param numLines
+ */
+export function drawHorizontalGridLines (ctx, canvas, numLines = 10) {
+  const verticalSpacing = canvas.height / numLines
+  for (let i = 0; i <= numLines; i++) {
+    ctx.beginPath()
+    ctx.moveTo(0, i * verticalSpacing)
+    ctx.lineTo(canvas.width, i * verticalSpacing)
+    ctx.strokeStyle = '#ddd'
+    ctx.stroke()
+  }
+}
+
+/**
+ *
+ * @param ctx
+ * @param canvas
+ * @param numLines
+ */
+export function drawVerticalGridLines (ctx, canvas, numLines = 10) {
+  const horizontalSpacing = canvas.width / numLines
+  for (let i = 0; i <= numLines; i++) {
+    ctx.beginPath()
+    ctx.moveTo(i * horizontalSpacing, 0)
+    ctx.lineTo(i * horizontalSpacing, canvas.height)
+    ctx.strokeStyle = '#ddd'
+    ctx.stroke()
+  }
+}
+
+/**
+ * Draw a rectangle on the canvas.
+ *
+ * @param {CanvasRenderingContext2D} ctx - The canvas context
+ * @param {number} x - The x-coordinate of the top-left corner
+ * @param {number} y - The y-coordinate of the top-left corner
+ * @param {number} width - The width of the rectangle
+ * @param {number} height - The height of the rectangle
+ * @param {string} color - The fill color of the rectangle
  */
 export function drawRectangle (ctx, x, y, width, height, color) {
   ctx.fillStyle = color
--- src/module/main.js
+++ src/module/main.js
@@ -1,12 +1,37 @@
+import { drawHorizontalGridLines, drawVerticalGridLines } from './drawUtils.js'
 import { drawBarGraph } from './graph.js'
 
 /**
  *
  */
 window.onload = function () {
+  test1()
+  test2()
+}
+
+/**
+ *
+ */
+function test1 () {
   const canvas = document.getElementById('myCanvas')
   const ctx = canvas.getContext('2d')
 
-  const data = [50, 100, 150, 200, 250]
-  drawBarGraph(ctx, data, 'red')
+  drawHorizontalGridLines(ctx, canvas)
+  drawVerticalGridLines(ctx, canvas)
+
+  const sampleData = [50, 100, 150, 200, 250]
+
+  drawBarGraph(ctx, sampleData, 'red')
+}
+
+function test2 () {
+  const canvas = document.getElementById('myCanvas2')
+  const ctx = canvas.getContext('2d')
+
+  drawHorizontalGridLines(ctx, canvas)
+  drawVerticalGridLines(ctx, canvas)
+
+  const sampleData = [200, 400, 150, 200, 250]
+
+  drawBarGraph(ctx, sampleData, 'red')
 }
